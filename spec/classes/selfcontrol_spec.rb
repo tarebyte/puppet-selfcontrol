569f625eefcda01bcc1db1dbaaa629402adf4618
Mark Tareshawty <tarebyte@gmail.com>
2014-05-05 20:33:55 -0400

    working tests

--- spec/classes/selfcontrol_spec.rb
+++ spec/classes/selfcontrol_spec.rb
@@ -2,9 +2,9 @@ require 'spec_helper'
 
 describe 'selfcontrol' do
   it do
-    should contain_anchor('SelfControl').with({
-      :source => 'http://downloads.selfcontrolapp.com/SelfControl-1.5.1.zip',
-      :provider => 'compressed_app'
+    should contain_package('SelfControl').with({
+      :provider => 'compressed_app',
+      :source => 'http://downloads.selfcontrolapp.com/SelfControl-1.5.1.zip'
     });
   end
 end
